32f8990d07b47de64032a43f48a1babf7d2d004f
Jack Sorensen <jack.sorensen11@gmail.com>
2020-01-12 21:31:34 -0800

    push after tutoring

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-// Set the clock
+
 var clock = $('#clock');
 function update() {
 	$('#clock').html(moment().format('MMMM DD YYYY H:mm:ss'));
 }
 setInterval(update, 1000);
 
-// Create all necessary elements
-var hourArry = ["9:00 a.m.", "10:00 a.m.", "11:00 a.m.", "12:00 p.m.", "1:00 p.m.", "2:00 p.m.", "3:00 p.m.", "4:00 p.m.", "5:00 p.m.", ]
+
+var hourArry = ["9:00 a.m.", "10:00 a.m.", "11:00 a.m.", "12:00 p.m.", "1:00 p.m.", "2:00 p.m.", "3:00 p.m.", "4:00 p.m.", "5:00 p.m.",]
 var container = $(".container");
 
 var date = moment("12/25/1995", "MM-DD-YYYY");
 
-// FOR MULTI DAY PLANNER ADD NAVIGATE FUNCTIONS: PREV, TODAY, NEXT, AND SEARCH FIELD
+
+var cityName = $("#cityName").val();
+console.log(cityName);
+
+const apiKey = "27428ea9a3d610198180273b120a59fc";
+// to call the api
+const ApiAddress = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},usa&appid=${apiKey}`;
 
 
-// Build timeblocks
-// FOR MULTI DAY PLANNER ADD FUNCTION TO BE CALLED ON LOAD AND WHEN NAVIGATE BUTTONS ARE PRESSED
+function getdata() {
+
+	$.ajax({
+		url: ApiAddress,
+		method: "GET"
+	})
+		.then(function (response) {
+			// console.log(response)
+
+
+			for (let i = 0; i < 5; i++) {
+
+
+
+
+				let forecastBox = $("#forecastBox");
+				var temp = response.list[i].main.temp;
+				let forecastDiv = $("<div/>");
+				let forecastUl = $("<ul>");
+
+				$(forecastUl).append(`<li>${temp}</li>`);
+
+
+				$(forecastUl).appendTo(forecastDiv);
+				$(forecastDiv).append(forecastUl);
+				$("#forecastBox").append(forecastDiv);
+			}
+		})
+
+}
+
+
+$("#search").on("click", function (e) {
+	e.preventDefault();
+	console.log("teting")
+	getdata();
+
+
+})
+
+
+
 for (let i = 0; i < hourArry.length; i++) {
-// New row for each block of time
 	var row = $("<div>");
 	row.addClass("row");
 	container.append(row);
 
-	// Time block with text of hour
+
 	var timeBlock = $("<div>");
 	timeBlock.addClass("time-block col-md-2");
 	timeBlock.text(hourArry[i]);
@@ -34,32 +79,30 @@ for (let i = 0; i < hourArry.length; i++) {
 	textArea.attr("placeholder", "What needs to be done?");
 	if (localStorage.getItem(hourArry[i]) !== null) {
 		textArea.text(localStorage.getItem(hourArry[i]))
-		
+
 	}
 	row.append(textArea);
-	
 
-	// Change style based on past, present, future
-	if (i+9 < parseInt(moment().format('HH'))) {
+
+
+	if (i + 9 < parseInt(moment().format('HH'))) {
 		textArea.addClass("past");
-	} 	else if (i+9 === parseInt(moment().format('HH'))) {
+	} else if (i + 9 === parseInt(moment().format('HH'))) {
 		textArea.addClass("present");
-	} 	else if (i+9 > parseInt(moment().format('HH'))) {
+	} else if (i + 9 > parseInt(moment().format('HH'))) {
 		textArea.addClass("future");
 	}
 
-	// Save button
+
 	var saveBtn = $("<button>");
 	saveBtn.addClass("saveBtn col-md-1");
 	saveBtn.text("Save");
 	row.append(saveBtn);
 
 }
-// }
 
-// Get user input
 $(document).ready(function () {
 	$(".saveBtn").click(function () {
-		localStorage.setItem($(this).parent().children("textarea").attr("id") , $(this).parent().children("textarea").val());
+		localStorage.setItem($(this).parent().children("textarea").attr("id"), $(this).parent().children("textarea").val());
 	});
 });
